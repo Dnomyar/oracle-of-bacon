de87103702c880b952981cfc045e718f7765d8fd
Damien RAYMOND <dam.ray49@gmail.com>
2018-02-21 11:01:17 +0100

    Implement get actor by name using mongo

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
@@ -6,15 +6,17 @@ import org.bson.Document;
 
 import java.util.Optional;
 
+import static com.mongodb.client.model.Filters.eq;
+
 public class MongoDbRepository {
     private final MongoCollection<Document> actorCollection;
 
     public MongoDbRepository() {
-        this.actorCollection= new MongoClient("localhost", 27017).getDatabase("workshop").getCollection("actors");
+        this.actorCollection = new MongoClient("localhost", 27017).getDatabase("workshop").getCollection("actors");
     }
 
     public Optional<Document> getActorByName(String name) {
-        // TODO implement actor fetch
-        return null;
+        return Optional.ofNullable(
+                this.actorCollection.find(eq("name", name)).first());
     }
 }
