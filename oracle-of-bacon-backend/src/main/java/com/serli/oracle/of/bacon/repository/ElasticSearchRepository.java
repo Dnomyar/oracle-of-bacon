abbb1413bde20b39f6d5675ca9e8deaaf7f4772c
Damien RAYMOND <dam.ray49@gmail.com>
2018-02-21 10:45:41 +0100

    Add actor suggest request

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/ElasticSearchRepository.java
@@ -1,10 +1,22 @@
 package com.serli.oracle.of.bacon.repository;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.suggest.Suggest;
+import org.elasticsearch.search.suggest.SuggestBuilder;
+import org.elasticsearch.search.suggest.SuggestBuilders;
+import org.elasticsearch.search.suggest.SuggestionBuilder;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
+import org.elasticsearch.search.suggest.term.TermSuggestion;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ElasticSearchRepository {
@@ -25,7 +37,49 @@ public class ElasticSearchRepository {
     }
 
     public List<String> getActorsSuggests(String searchQuery) throws IOException {
-        // TODO implement suggest
-        return null;
+
+        System.out.println("Getting actors suggest from token '" + searchQuery + "'");
+
+        RestHighLevelClient client = createClient();
+
+        String SUGGESTION_NAME = "suggest_actor_name";
+        String ACTOR_NAME_FIELD = "name";
+        String SUGGEST_FIELD = "suggest";
+
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+//        SuggestionBuilder termSuggestionBuilder =
+//                SuggestBuilders.termSuggestion(SUGGEST_NAME_FIELD).text(searchQuery);
+        SuggestionBuilder completionSuggestionBuilder =
+                SuggestBuilders.completionSuggestion(SUGGEST_FIELD).text(searchQuery);
+        SuggestBuilder suggestBuilder = new SuggestBuilder();
+        suggestBuilder.addSuggestion(SUGGESTION_NAME, completionSuggestionBuilder);
+        searchSourceBuilder.suggest(suggestBuilder);
+
+        SearchRequest searchRequest = new SearchRequest().source(searchSourceBuilder);
+
+        SearchResponse searchResponse = client.search(searchRequest);
+
+
+        List<String> suggestions = new LinkedList<>();
+
+        CompletionSuggestion termSuggestion =
+                searchResponse.getSuggest().getSuggestion(SUGGESTION_NAME);
+
+        for (CompletionSuggestion.Entry entry : termSuggestion.getEntries()) {
+            for (CompletionSuggestion.Entry.Option option : entry.getOptions()) {
+
+                String suggestText =
+                        option
+                                .getHit()
+                                .getSourceAsMap()
+                                .get(ACTOR_NAME_FIELD)
+                                .toString();
+
+                suggestions.add(suggestText);
+            }
+        }
+
+        return suggestions;
     }
 }
